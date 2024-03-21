41a8858dabac2bbff501ece3f0613c16ec9d2b57
Lucas Boscatti <lucas.boscatti@e-core.com>
2024-03-21 20:11:10 -0300

    Add model_option and game_mode arguments to EmotionDetector class

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import argparse
 
+import torch
+
 from emotion_detector import EmotionDetector
+
+CUDA = torch.cuda.is_available()
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Lucas Boscatti's Final Project Nero")
     parser.add_argument("--model_name", default="resnet18.onnx", type=str)
+    parser.add_argument("--model_option", default="onnx", type=str)
     parser.add_argument(
         "--backend_option",
-        default=1,
+        default=2 if CUDA else 1,
         type=int,
         choices=[1, 2, 3],
-        help="Choose the backend-target pair to run this demo: "
-        "1: OpenCV implementation + CPU, "
-        "2: CUDA + GPU (CUDA), "
-        "3: CUDA + GPU (CUDA FP16)",
+        help="""Choose the backend-target pair to run this demo:
+        1: OpenCV implementation + CPU,
+        2: CUDA + GPU (CUDA)
+        3: CUDA + GPU (CUDA FP16)""",
     )
     parser.add_argument(
         "--providers",
-        default=None,
+        default=2 if CUDA else 1,
         type=int,
         choices=[1, 2, 3],
-        help="Choose the backend-target pair to run this demo: "
-        "1: CPUExecutionProvider, "
-        "2: CUDAExecutionProvider (CUDA), "
-        "3: TensorrtExecutionProvider (CUDA FP16)",
+        help="""Choose the backend-target pair to run this demo:
+        1: CPUExecutionProvider
+        2: CUDAExecutionProvider (CUDA)
+        3: TensorrtExecutionProvider (CUDA FP16)""",
     )
     parser.add_argument(
         "--file",
-        default=0,
+        default="0",
         type=str,
-        help="Specify the input source: 0, 'realsense', or 'path/to/video or image'",
+        help="""Specify the input source: 
+        '0' to default webcam
+        'realsense' to use the realsense camera
+        'path_to/video' to use a video or a image file""",
     )
     parser.add_argument(
         "--display_window",
@@ -46,7 +54,10 @@ def parse_arguments():
         help="Specify the processing option: 'video' or 'image'",
     )
     parser.add_argument(
-        "--num_faces", default=None, type=int, help="Number of faces to detect"
+        "--game_mode",
+        default=False,
+        type=bool,
+        help="Specify the game mode: True or False. If True, only two faces will be detected and a output of the winner will be displayed",
     )
 
     return parser.parse_args()
@@ -56,16 +67,14 @@ def main():
     args = parse_arguments()
 
     detector = EmotionDetector(
-        backend_option=args.backend_option,
         model_name=args.model_name,
+        model_option=args.model_option,
+        backend_option=args.backend_option,
         providers=args.providers,
-        num_faces=args.num_faces,
+        game_mode=args.game_mode,
     )
 
-    if args.option == "image":
-        detector.process_image(args.file)
-
-    detector.process_video(args.file, display_window=args.display_window)
+    detector.start_inference(args.file, display_window=args.display_window)
 
 
 if __name__ == "__main__":
