7e5597bcfa5e311301146f2149cf6bbb1ec74a8d
Lucas Boscatti <lucas.boscatti@e-core.com>
2024-04-11 11:41:58 -0300

    Refactor video_feed function in main.py and update requirements.txt

--- main.py
+++ main.py
@@ -144,5 +144,5 @@ def video_feed():
         mimetype="multipart/x-mixed-replace; boundary=frame",
     )
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
