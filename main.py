105295929580a6b45f5cccf446916014435af11a
Lucas Boscatti <lucas.boscatti@e-core.com>
2024-04-11 10:22:24 -0300

    flask templates

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import argparse
-
 import torch
-from flask import Flask, Response, render_template
+from flask import Flask, Response, redirect, render_template, request, url_for
 
 from emotion_detector import EmotionDetector
 
@@ -9,82 +7,138 @@ CUDA = torch.cuda.is_available()
 app = Flask(__name__, template_folder="template")
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Lucas Boscatti's Final Project Nero")
-    parser.add_argument("--model_name", default="resnet18.onnx", type=str)
-    parser.add_argument(
-        "--model_option",
-        default="onnx",
-        type=str,
-        choices=["pytorch", "onnx", "tensorrt"],
-        help="""Choose the model to run:
-        1: pytorch
-        2: onnx
-        3: tensorrt""",
-    )
-    parser.add_argument(
-        "--backend_option",
-        default=2 if CUDA else 1,
-        type=int,
-        choices=[1, 2, 3],
-        help="""Choose the backend-target pair to run this demo:
-        1: OpenCV implementation + CPU,
-        2: CUDA + GPU (CUDA)
-        3: CUDA + GPU (CUDA FP16)""",
-    )
-    parser.add_argument(
-        "--providers",
-        default=2 if CUDA else 1,
-        type=int,
-        choices=[1, 2, 3],
-        help="""Choose the backend-target pair to run this demo:
-        1: CPUExecutionProvider
-        2: CUDAExecutionProvider (CUDA)
-        3: TensorrtExecutionProvider (CUDA FP16)""",
-    )
-    parser.add_argument(
-        "--file",
-        default="0",
-        type=str,
-        choices=["0", "realsense", "path_to/video"],
-        help="""Specify the input source: 
-        '0' to default webcam
-        'realsense' to use the realsense camera. 
-        'path_to/video' to use a video or a image file""",
-    )
-    parser.add_argument(
-        "--game_mode",
-        default=False,
-        type=bool,
-        help="Specify the game mode: True or False. If True, only two faces will be detected and a output of the winner will be displayed",
-    )
-
-    return parser.parse_args()
+class DeviceWindows:
+    def __init__(
+        self,
+        model_name="resnet18.onnx",
+        model_option="onnx",
+        backend_option=0,
+        providers=1,
+    ):
+        self.model_name = model_name
+        self.model_option = model_option
+        self.backend_option = backend_option
+        self.providers = providers
 
 
-@app.route("/")
+class DeviceJetsonNano:
+    def __init__(
+        self,
+        model_name="resnet18.engine",
+        model_option="tensorrt",
+        backend_option=1,
+        providers=1,
+    ):
+        self.model_name = model_name
+        self.model_option = model_option
+        self.backend_option = backend_option
+        self.providers = providers
+
+
+class DeviceLinux:
+    def __init__(
+        self,
+        model_name="resnet18.engine",
+        model_option="tensorrt",
+        backend_option=3,
+        providers=1,
+    ):
+        self.model_name = model_name
+        self.model_option = model_option
+        self.backend_option = backend_option
+        self.providers = providers
+
+
+class DeviceCustom:
+    def __init__(
+        self,
+        model_name,
+        model_option,
+        backend_option,
+        providers,
+    ):
+        self.model_name = model_name
+        self.model_option = model_option
+        self.backend_option = backend_option
+        self.providers = providers
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Video streaming home page."""
+    if request.method == "POST":
+        selected_device = request.form["device_option"]
+        video_option = request.form["video_option"]
+        if selected_device == "windows":
+            device = DeviceWindows()
+        elif selected_device == "jetson_nano":
+            device = DeviceJetsonNano()
+        elif selected_device == "linux":
+            device = DeviceLinux()
+        elif selected_device == "custom":
+            return redirect(
+            url_for("select_params", video_option=video_option))
+        return redirect(
+            url_for(
+                "video_feed",
+                model_name=device.model_name,
+                model_option=device.model_option,
+                backend_option=device.backend_option,
+                providers=device.providers,
+                video_option=video_option,
+            )
+        )
     return render_template("index.html")
 
 
-@app.route("/video_feed")
+@app.route("/select_params/<video_option>", methods=["GET", "POST"])
+def select_params(video_option):
+    if request.method == "POST":
+        model_option = request.form["model_option"]
+        backend_option = request.form["backend_option"]
+        providers = request.form["providers"]
+
+        model_names = {
+            "pytorch": "best_checkpoint.tar",
+            "onnx": "resnet18.onnx",
+            "engine": "resnet18.engine",
+        }
+
+        return redirect(
+            url_for(
+                "video_feed",
+                model_name=model_names[model_option],
+                model_option=model_option,
+                backend_option=backend_option,
+                providers=providers,
+                video_option=video_option,
+            )
+        )
+    return render_template("select_params.html")
+
+
+@app.route("/video_feed", methods=["GET", "POST"])
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
+    args = request.args.to_dict()
+    detector = EmotionDetector(
+        model_name=args["model_name"],
+        model_option=args["model_option"],
+        backend_option=int(args["backend_option"]),
+        providers=int(args["providers"]),
+        video_option=args["video_option"],
+    )
     return Response(
-        detector.start_inference(args.file),
+        detector.start_inference(),
         mimetype="multipart/x-mixed-replace; boundary=frame",
     )
 
 
-args = parse_arguments()
-
-detector = EmotionDetector(
-    model_name=args.model_name,
-    model_option=args.model_option,
-    backend_option=args.backend_option,
-    providers=args.providers,
-    game_mode=args.game_mode,
-)
+# detector = EmotionDetector(
+#     model_name=args.model_name,
+#     model_option=args.model_option,
+#     backend_option=args.backend_option,
+#     providers=args.providers,
+# )
 
 app.run(debug=True)
